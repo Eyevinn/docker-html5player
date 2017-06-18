328caf397cf587da9d3697abdf939a16a77ec7e0
Jonas Birme <jonas.birme@gmail.com>
2017-06-18 08:58:26 +0200

    Started with a gulp task to build minified js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp'),
   browserify = require('browserify'),
   babelify = require('babelify'),
+  uglify = require('gulp-uglify'),
+  source = require('vinyl-source-stream'),
+  rename = require('gulp-rename'),
   fs = require('fs');
 
 const libs = ['./js/lib/shaka-player.compiled.js'];
@@ -16,6 +19,24 @@ gulp.task('build', () => {
     ignore: /\js\/lib\//
   })
   .bundle()
-  .pipe(fs.createWriteStream('public/js/player.js'));
+  .pipe(source('player.js'))
+  .pipe(gulp.dest('public/js/'))
 });
 
+gulp.task('prod', () => {
+  const b = browserify({
+    entries: [ 'js/index.js' ],
+    noParse: libs,
+    debug: true,
+  })
+  .transform(babelify, {
+    presets: [ 'es2015' ],
+    ignore: /\js\/lib\//
+  })
+  .bundle()
+  .pipe(source('player.js'))
+  .pipe(rename('player' + '-' + process.env.npm_package_version + '.js'))
+  .pipe(rename({ extname: ".min.js" }))
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js/'))
+});
